21a5b4ca3dcfeb94eb82af2714c0d09ff85a6ad4
Mubashirsw27 <mubashirkhan786bhai@gmail.com>
2020-10-17 10:42:07 +0500

    Java Polymorphism Updated

---
+++ Java Polymorphism/Dog.java
@@ -0,0 +1,9 @@
+public class Dog extends Pets { //Contains a same void Method "name" as Pets.java
+
+  /*Note : Keyword "extends"  is used for Inheritance.
+It is useful for code reusability: reuse attributes 
+and methods of an existing class when you create a new class.*/
+  public void name() {
+    System.out.println("Dog says : Bhaaow");
+  }
+}
---
+++ Java Polymorphism/Pets.java
@@ -0,0 +1,6 @@
+public class Pets {//Contains a method "name"
+
+    public void name() {
+      System.out.println("Pets have a language");
+    }
+  }
