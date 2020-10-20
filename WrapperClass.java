7a6247a7d70e0d0db6b4253db3f67146abe3b65d
LaibaMemon <49434426+LaibaMemon@users.noreply.github.com>
2020-10-20 17:53:23 +0500

    Update WrapperClass.java

--- WrapperClass.java
+++ WrapperClass.java
@@ -29,3 +29,30 @@ public class WrapperClass {
     }
     
 }
+
+class Wrapperclass
+{
+	public static void main(String args[])
+	{
+	   String a="123";
+	   Integer i=Integer.parseInt(a);
+
+	   if(a.equals("123"))
+	   {
+	       System.out.println("Equals");
+	   }
+	   else
+	   {
+	      System.out.println("Not equals");
+	   }
+
+	   if(i==(123))
+	   {
+	   	  System.out.println("Print");
+	   }
+	   else
+	   {
+	   	  System.out.println("Not Print");
+	   }
+	}
+}
