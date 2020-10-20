2bc825e28ccc555a7f005362144ba80d8d2829ad
LaibaMemon <49434426+LaibaMemon@users.noreply.github.com>
2020-10-20 18:06:16 +0500

    Update ForLoopExample1.java

--- Loops/For Loop/ForLoopExample1.java
+++ Loops/For Loop/ForLoopExample1.java
@@ -7,3 +7,25 @@ public class InfiniteLoop {
          }
     }
 }
+
+class ForEvenOrOdd
+{
+public static void main(String args[])
+{
+  int i,j;
+  for(i=0;i<=10;i++)
+{
+  if(i%2==1)
+{ 
+  System.out.println("Odd Numbers:" +i);
+}
+}
+   for(j=0;j<10;j++)
+{ 
+  if(j%2==0)
+{ 
+  System.out.println("Even Number:"+j);
+}
+}
+}
+}
