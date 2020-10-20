62be9d558cc9e88ca6a50a5214625e47c70132b9
Waqar Ali Siyal <sirai.waqar.ali@gmail.com>
2020-10-20 18:49:51 +0500

    Added Hash Tables

---
+++ Data Structures & Algorithm/HashTables/3. Double Hashing/HashTable.java
@@ -0,0 +1,128 @@
+class HashTable implements Map{
+    private Entry[] entries;
+    private int size;
+    private int used;
+    private float loadFactor;
+    private final Entry NIL = new Entry(null,null);   //dummy
+
+    public HashTable(int capacity,float loadFactor){
+        entries = new Entry[capacity];
+        this.loadFactor = loadFactor;
+    }
+    public HashTable(int capacity){
+        this(capacity,0.75F);
+    }
+    public HashTable(){
+        this(101);
+    }
+
+    private class Entry{
+        Object key;
+        Object value;
+        Entry(Object key,Object value){
+            this.key=key;
+            this.value=value;
+        }
+    }
+
+    public Object get(Object key){
+        int h = hash(key);
+        int d = hash2(key);
+        for(int i=0;i<entries.length;i++){
+            int j = nextProbe(h,d,i);
+            Entry entry = entries[j];
+            if(entry==null)
+                break; 
+            if(entry==NIL)
+                continue;
+            if(entry.key.equals(key))
+                return entry.value;   //success
+        }
+        return null;   //failure: key not found
+    }
+
+    public Object put(Object key,Object value){
+        if(used>loadFactor*entries.length)
+            rehash();
+        int h = hash(key);
+        int d = hash2(key);
+        for(int i=0;i<entries.length;i++){
+            int j = nextProbe(h,d,i);
+            Entry entry = entries[j];
+            if(entry==null){
+                entries[j] = new Entry(key,value);
+                ++size;
+                ++used;
+                return null;   //insertion success
+            }
+            if(entry==NIL)
+                continue;
+            if(entry.key.equals(key)){
+                Object oldValue = entry.value;
+                entries[j].value = value;
+                return oldValue;    //update success
+            }
+        }
+        return null;    // failure: table overflow
+    }
+    
+    public Object remove(Object key){
+        int h = hash(key);
+        int d = hash2(key);
+        for(int i=0;i<entries.length;i++){
+            int j = nextProbe(h,d,i);
+            Entry entry = entries[j];
+            if(entry==null)
+                break; 
+            if(entry==NIL)
+                continue;
+            if(key.equals(entry.key)){
+                Object value = entry.value;
+                --size;
+                entries[j]=NIL;   //dummy value
+                return value;    //success
+            }
+        }
+        return null;    //failure: key not found
+    }
+
+    public int size(){
+        return size;
+    }
+
+    private int hash(Object key){
+        if(key==null)
+            throw new IllegalArgumentException();
+        return (key.hashCode() & 0x7FFFFFFF)%entries.length;
+    }
+
+    private int hash2(Object key){
+        if(key==null)
+            throw new IllegalArgumentException();
+        return 1+(key.hashCode() & 0x7FFFFFFF)%(entries.length-1);
+    }
+
+    private int nextProbe(int h,int d,int i){
+        return (h+i*d)%entries.length;
+    }
+
+    private void rehash(){
+        Entry[] oldEntries = entries;
+        entries = new Entry[2*entries.length+1];
+        for(int k=0;k<oldEntries.length;k++){
+            Entry entry = oldEntries[k];
+            if(entry==null || entry==NIL)
+                continue;
+            int h = hash(entry.key);
+            int d = hash2(entry.key);
+            for(int i=0;i<entries.length;i++){
+                int j = nextProbe(h,d,i);
+                if(entries[j]==null){
+                    entries[j] = entry;
+                    break;
+                }
+            }
+        }
+        used=size;
+    }
+}
