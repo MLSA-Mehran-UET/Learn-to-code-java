913186643177ae159d6d9346d34fa5694d165dad
LaibaMemon <49434426+LaibaMemon@users.noreply.github.com>
2020-10-20 18:13:01 +0500

    Update MultipleCatch.java

--- ExceptionHandling/MultipleCatch.java
+++ ExceptionHandling/MultipleCatch.java
@@ -24,3 +24,29 @@ public class MultipleCatch {
                System.out.println("rest of the code");    
     }  
 } 
+
+class MultipleCatch
+{
+	public static void main(String args[])
+	{
+	   int []num1,num2;
+
+	   try
+	   {
+	      num1=new int[5];
+	      System.out.println(num1[5]);
+	   }
+	  catch(NullPointerException a)
+	   {
+	     System.out.println("zero");
+	   }
+
+	   catch(Exception b)
+	   {
+	    System.out.println("Bound to index");
+	   }
+
+
+	    System.out.println("Out off catch");
+	}
+}
