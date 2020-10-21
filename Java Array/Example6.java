3b9b97ef22faacccc45ee1b46e6dacd332db329f
Javeria Tabassum <50498182+javeriatabassum145@users.noreply.github.com>
2020-10-21 17:37:58 +0500

    Update Example6.java

--- Java Array/Example6.java
+++ Java Array/Example6.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
 
-//        Creating 2-D array
+//        Creating 3-D array
         int[][][] array = {{{10,20,30},{40,50,60},{70,80,90}}};
 //     Extracting elements using for-each loop
         for (int[][] i : array){
