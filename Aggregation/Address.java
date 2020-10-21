f0710f34673ee0e8590ab5c5e30738cfd63fcd44
Saqib-ghouri11 <Saqib-ghouri11@users.noreply.github.com>
2020-10-21 19:48:12 +0500

    Aggregation in Java
    Aggregation in Java is a relationship between two classes that is best described as a "has-a" and "whole/part" relationship. It is a more specialized version of the association relationship. The aggregate class contains a reference to another class and is said to have ownership of that class.

---
+++ Aggregation/Address.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aggregation;
+
+/**
+ *
+ * @author Ahsan Qadeer
+ */
+public class Address {
+    String city, province, country;
+    public Address (String city, String province, String country)
+    {
+        this.city=city;
+        this.province=province;
+        this.country=country;
+    }
+}
