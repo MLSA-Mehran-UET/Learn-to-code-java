5befdbc903e3f99514b743a4fe90b2704346bf59
Javeria Tabassum <50498182+javeriatabassum145@users.noreply.github.com>
2020-10-21 17:13:38 +0500

    Update Javaarray.java

--- Java Array/Javaarray.java
+++ Java Array/Javaarray.java
@@ -3,13 +3,18 @@ class Javaarray {
      
       // create an array
       int[] age = {12, 4, 5, 2, 5};
+        
+//       Accessing elements using for loop
+      for (int i = 0 ; i < age.length ; i++){
+            System.out.println("Element on index " + i + " is " + age[i]);
+      }
    
       // access each array elements
-      System.out.println("Accessing Elements of Array:");
-      System.out.println("First Element: " + age[0]);
-      System.out.println("Second Element: " + age[1]);
-      System.out.println("Third Element: " + age[2]);
-      System.out.println("Fourth Element: " + age[3]);
-      System.out.println("Fifth Element: " + age[4]);
+//       System.out.println("Accessing Elements of Array:");
+//       System.out.println("First Element: " + age[0]);
+//       System.out.println("Second Element: " + age[1]);
+//       System.out.println("Third Element: " + age[2]);
+//       System.out.println("Fourth Element: " + age[3]);
+//       System.out.println("Fifth Element: " + age[4]);
     }
    }
