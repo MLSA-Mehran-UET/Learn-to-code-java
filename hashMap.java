91f83939d6c62265c7e35d010c224142a5a3b0d6
Muhammad Sameer Khan <63584585+sameerkhan00@users.noreply.github.com>
2020-10-22 11:13:36 +0500

    Create hashMap.java

---
+++ hashMap.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrayprog;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Sameer
+ */
+public class hashmap {
+    public static void main(String[] args) {
+
+   HashMap<String, String> name = new HashMap<String, String>();
+
+    
+name.put("Sameer  ", " khan");
+    name.put("majd", "odeh");
+    name.put("asif", "shaikh");
+    name.put("kashif", "rajput");
+    System.out.println(name);
+  }
+}
+
+    
+
