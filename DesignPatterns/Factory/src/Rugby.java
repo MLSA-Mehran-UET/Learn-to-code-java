1354b7872b671803043ed4f7656ea2976a7b8c2d
sourcedilee <dileepacctv@gmail.com>
2020-10-24 01:49:01 +0530

    adding factory design pattern example

---
+++ DesignPatterns/Factory/src/Rugby.java
@@ -0,0 +1,11 @@
+public class Rugby extends Sport{
+
+    public Rugby() {
+        this.code = Sport.RUGBY;
+    }
+
+    @Override
+    public String description() {
+        return "Rugby is popular game and A match is played with 2 teams and each team should have maximum 15 players.";
+    }
+}
---
+++ DesignPatterns/Factory/src/VolleyBall.java
@@ -0,0 +1,11 @@
+public class VolleyBall extends Sport {
+
+    public VolleyBall() {
+        this.code = Sport.VOLLEYBALL;
+    }
+
+    @Override
+    public String description() {
+        return "VolleyBall is popular game and A match is played with 2 teams and each team should have maximum 6 players.";
+    }
+}
